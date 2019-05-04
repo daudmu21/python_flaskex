30d7969118d91df34997f02312c19f6ee7dd0296
daudmu21 <daudmuhudin@gmail.com>
2019-05-04 15:16:36 -0500

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 node{
-    properties([parameters([string(defaultValue: 'IP', description: 'where to build e.g IP', name: 'ENV'"", trim: true)])])
+     properties([parameters([string(defaultValue: 'IP', description: 'Where to build e.g IP', name: 'ENV', trim: true)])])
     stage("clone repo"){
         git 'git@github.com:daudmu21/Flaskex.git'
     }
-    stage ("Build application"){
-        sh "scp-r * ssh ec2-user@${ENV}:/tmp"
+        stage("Build application"){
+        sh "scp -r * ec2-user@${ENV}:/tmp"
         sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
     }
     stage("App Run"){
-        sh "ssh ec2-user@${ENV} python /tmp/app.py"
+        sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
     }
 }
